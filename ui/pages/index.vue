074d4cb4cc1367634bbcfb3691ecbe1d4f3eaf15
tlacloc <desneruda@gmail.com>
2023-06-19 11:28:51 -0600

    ✨ feat(index.vue): add header, main content, speakers, date and location, form and footer sections to improve user experience 🎨 style(index.vue): add background color to footer to improve visual design

--- ui/pages/index.vue
+++ ui/pages/index.vue
@@ -1,5 +1,94 @@
 <template>
-  <Tutorial />
+  <div>
+    <head>
+      <title>Mayita Accesorios</title>
+    </head>
+
+    <body data-spy="scroll" data-target="#navbarSupportedContent" data-offset="57">
+      <!-- Header -->
+      <header>
+        Hola
+      </header>
+      <!-- End of header -->
+
+      <!-- main content -->
+      <!-- End of main content -->
+
+      <!-- Speakers -->
+      <!-- End of content -->
+
+      <!-- Date and location -->
+      <!-- End of date and location -->
+
+      <!-- Form -->
+      <!-- End of form -->
+
+      <!-- footer -->
+      <footer id="footer" class="pb-4 pt-4">
+        <div class="container">
+          <div class="row text-center">
+            <div class="col-12 col-lg">
+              <h5>Recibe nuestras promociones</h5>
+            </div>
+          </div>
+          <div class="row text-center">
+            <div class="col-12 col-lg">
+              <h6>
+                Enterate de nuestras promociones y nuevos productos
+              </h6>
+              <form>
+                <div class="form-group">
+                  <input id="exampleInputEmail1" type="email" class="form-control" aria-describedby="emailHelp" placeholder="Ingresa tu correo electrónico">
+                </div>
+                <button type="submit" class="btn btn-primary">
+                  Enviar
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <h5 class="text-left">
+                Mas información
+              </h5>
+              <h6 class="text-left">
+                Envios
+              </h6>
+              <h6 class="text-left">
+                Cambios y devoluciones
+              </h6>
+              <h6 class="text-left">
+                ¿Cuál es mi talla?
+              </h6>
+              <h6>
+                Cuidados a mi joyería
+              </h6>
+              <h6>
+                Kueski Pay
+              </h6>
+              <h6>
+                Mayoreo
+              </h6>
+              <h6>
+                Aviso de privacidad
+              </h6>
+            </div>
+            <div class="col">
+              <h5 class="text-left">
+                Contactanos
+              </h5>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <!-- End of footer -->
+
+      <!-- Modal -->
+      <!-- End of modal -->
+    </body>
+  </div>
 </template>
 
 <script>
@@ -7,3 +96,9 @@ export default {
   name: 'IndexPage'
 }
 </script>
+
+<style>
+#footer {
+  background-color: #DAA1C7;
+}
+</style>
